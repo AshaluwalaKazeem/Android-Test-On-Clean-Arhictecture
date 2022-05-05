8c4e2ce55ecd5bfd4ce0eeee89f6161aca188a46
ashaluwalakazeem <ashaluwalakazeem@gmail.com>
2022-05-05 21:40:16 +0100

    - Implement Fetch API use case - Implement Save Note to Local Db use case

--- app/src/main/java/com/example/punchandroidtest/domain/use_case/fetch_api/FetchApiUseCase.kt
+++ app/src/main/java/com/example/punchandroidtest/domain/use_case/fetch_api/FetchApiUseCase.kt
@@ -1,6 +1,23 @@
 package com.example.punchandroidtest.domain.use_case.fetch_api
 
-class FetchApiUseCase {
+import com.example.punchandroidtest.common.Resource
+import com.example.punchandroidtest.domain.model.Mars
+import com.example.punchandroidtest.domain.repository.MarsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
+class FetchApiUseCase
+@Inject
+constructor(
+    private val marsRepository: MarsRepository
+)
+{
 
+    operator fun invoke(): Flow<Resource<List<Mars>>> = flow {
+        emit(Resource.Loading())
+        // Fetch data
+        val resource = marsRepository.get()
+        emit(resource)
+    }
 }
--- app/src/main/java/com/example/punchandroidtest/domain/use_case/save_note/SaveNoteToDbUseCase.kt
+++ app/src/main/java/com/example/punchandroidtest/domain/use_case/save_note/SaveNoteToDbUseCase.kt
@@ -1,4 +1,23 @@
 package com.example.punchandroidtest.domain.use_case.save_note
 
-class SaveNoteToDbUseCase {
+import com.example.punchandroidtest.common.Resource
+import com.example.punchandroidtest.domain.model.Mars
+import com.example.punchandroidtest.domain.repository.MarsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import javax.inject.Inject
+
+class SaveNoteToDbUseCase
+@Inject
+constructor(
+    private val marsRepository: MarsRepository
+)
+{
+    operator fun invoke(mars: Mars): Flow<Resource<Mars>> = flow {
+        emit(Resource.Loading())
+        // Save
+        val resource = marsRepository.save(mars)
+        emit(resource)
+    }
 }
